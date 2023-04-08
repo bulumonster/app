8817ba46572d3ba16757edd6e4390be4608557c8
bulumonster <130229704+bulumonster@users.noreply.github.com>
2023-04-08 21:30:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,37 +2,47 @@
 const form = document.querySelector('#form');
 const inputUrl = document.querySelector('#url');
 const resultDiv = document.querySelector('#result');
+const { apiEndpoint, appId, appKey } = config;
 
 form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	const url = inputUrl.value;
+  event.preventDefault();
+  const url = inputUrl.value;
 
-	if (url.indexOf('shopee.vn') < 0) {
-		resultDiv.innerText = 'Invalid Shopee link';
-		return;
-	}
+  if (url.indexOf('shopee.vn') < 0) {
+    resultDiv.innerText = 'Invalid Shopee link';
+    return;
+  }
 
-	fetch(`${config.apiEndpoint}?url=${encodeURIComponent(url)}`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Partner ${config.partnerId}:${config.partnerKey}`,
-			'Shops': config.shopId
-		},
-	})
-	.then(response => response.json())
-	.then(data => {
-		if (data.error) {
-			resultDiv.innerText = data.error_description;
-		} else {
-			resultDiv.innerHTML = `
-				<p>Short Link:</p>
-				<a href="${data.data.url}" target="_blank">${data.data.url}</a>
-			`;
-		}
-	})
-	.catch(error => {
-		console.error(error);
-		resultDiv.innerText = 'Error getting short link';
-	});
+  const timestamp = Math.floor(Date.now() / 1000);
+  const signature = `${apiEndpoint}|${timestamp}`;
+
+  fetch(`${apiEndpoint}?url=${encodeURIComponent(url)}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${appId}.${timestamp}.${generateSignature(signature, appKey)}`,
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.error) {
+        resultDiv.innerText = data.error_description;
+      } else {
+        resultDiv.innerHTML = `
+          <p>Short Link:</p>
+          <a href="${data.data.url}" target="_blank">${data.data.url}</a>
+        `;
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      resultDiv.innerText = 'Error getting short link';
+    });
 });
+
+function generateSignature(message, key) {
+  const crypto = require('crypto');
+  const hmac = crypto.createHmac('sha256', key);
+  hmac.update(message);
+  return hmac.digest('hex');
+}
