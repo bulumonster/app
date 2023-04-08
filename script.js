61164333db6726bf7f6e1339075827bb7e5a5859
bulumonster <130229704+bulumonster@users.noreply.github.com>
2023-04-08 21:16:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const shortLinkDiv = document.querySelector('#shortLink');
-const apiEndpoint = 'https://shopee.vn/api/v4/marketing/campaigns/url';
+import config from './config.js';
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault();
+const form = document.querySelector('#shorten-form');
+const input = document.querySelector('#original-link');
+const output = document.querySelector('#short-link');
+const button = document.querySelector('#shorten-button');
+const error = document.querySelector('#error-message');
 
-  const url = input.value;
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  output.textContent = '';
+  error.textContent = '';
+  button.disabled = true;
 
-  if (!url) {
-    return;
-  }
+  const originalLink = input.value;
 
   try {
-    const response = await fetch(apiEndpoint, {
+    const response = await fetch(config.apiEndpoint, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ url }),
+      body: JSON.stringify({ url: originalLink })
     });
 
     const data = await response.json();
 
-    const shortLink = data.data.short_url;
-
-    shortLinkDiv.textContent = shortLink;
-    shortLinkDiv.style.display = 'block';
-  } catch (error) {
-    console.error(error);
+    if (data.data.short_url) {
+      output.textContent = data.data.short_url;
+    } else {
+      throw new Error('Unable to get short link');
+    }
+  } catch (e) {
+    error.textContent = e.message;
+  } finally {
+    button.disabled = false;
   }
 });
