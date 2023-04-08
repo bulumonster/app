e903021371f52677393a5b599684b06ac051c72f
bulumonster <130229704+bulumonster@users.noreply.github.com>
2023-04-08 21:28:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const config = {
-  apiEndpoint: 'https://shopee.vn/api/v4/marketing/campaigns/url'
+  apiEndpoint: 'https://shopee.vn/api/v4/marketing/campaigns/url',
+  appId: '17385400032',
+  appKey: 'XYNFPI7P44LWNS67AERLYJZQJ3BOUPVR',
 };
 
 export default config;
